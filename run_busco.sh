31a043dc3fde19cc89459618620eecbbf44ca4af
Alyssa Vanerelli <93807021+alyssavanerelli@users.noreply.github.com>
2024-02-12 16:13:15 -0500

    Update run_busco.sh

--- run_busco.sh
+++ run_busco.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-FILES=$(ls -1 /projects/f_geneva_1/alyssa/grahami/busco/busco_phylogenomics/genomes/new/*.fa | cut -d "/" -f 10 | sort)
+FILES=$(ls -1 [YOUR PATH HERE]/busco/genomes/*.fa | cut -d "/" -f 10 | sort)
 for FILE in $FILES
         do
 	CMD="sbatch busco.sh ${FILE}"
