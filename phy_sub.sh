9392a73d7f916eaa5ecbfeb8c37b2f71f7076ba6
Alyssa Vanerelli <93807021+alyssavanerelli@users.noreply.github.com>
2024-02-14 13:29:15 -0500

    Update phy_sub.sh

--- phy_sub.sh
+++ phy_sub.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
-#SBATCH --partition=p_ccib_1
+#SBATCH --partition=cmain
 #SBATCH --exclude=gpuc001,gpuc002
 #SBATCH --job-name=buscophy
 #SBATCH --output=[YOUR PATH HERE]/busco/slurmout/slurm-%j-%x.out
-#SBATCH --mem=180G
+#SBATCH --mem=20G
 #SBATCH -n 20
 #SBATCH -N 1
-#SBATCH --time=7-00:00:00
+#SBATCH --time=3-00:00:00
 #SBATCH --requeue
 #SBATCH --mail-user=[YOUR NETID]@rutgers.edu
 #SBATCH --mail-type=FAIL
