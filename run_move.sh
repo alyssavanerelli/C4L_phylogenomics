88939aa634d30cf2d78575f28bab3d0a71c1b449
Alyssa Vanerelli <93807021+alyssavanerelli@users.noreply.github.com>
2024-03-04 15:59:37 -0500

    Delete run_move.sh

--- run_move.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-FILES=$(ls -1 [YOUR PATH HERE]/busco/genomes/*.fa | cut -d "/" -f 10 | sort)
-for FILE in $FILES
-        do
-	CMD="sbatch move.sh ${FILE}"
-        echo $CMD
-        #eval $CMD
-        sleep 0.25
-done
